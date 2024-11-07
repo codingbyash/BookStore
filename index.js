b939b13cf8c6903cc51eca83814800812c9988a0
ashishkr <singhashishgg2002@gmail.com>
2024-11-07 20:54:46 +0530

    change in some files

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ import { fileURLToPath } from 'url';
 
 import bookRoute from './route/book.route.js';
 import userRoute from './route/user.route.js';
-import couponRoute from "./route/coupon.route.js";
-// import addressRoute from "./route/address.route.js"
-import blogRoute from "./route/blogRoutes.js";
-import orderRoutes from "./route/order.route.js";
+import couponRoute from './route/coupon.route.js';
+import blogRoute from './route/blogRoutes.js';
+import orderRoutes from './route/order.route.js';
 
 const app = express();
 app.use(cors());
@@ -29,19 +28,19 @@ mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use('/book', bookRoute);
 app.use('/user', userRoute);
 app.use('/blogs', blogRoute);
-app.use('/coupons', couponRoute); // Use coupon routes
+app.use('/coupons', couponRoute);
 app.use('/api', orderRoutes);
-// app.use('/addresses', addressRoute); // Use address routes
 
-app.get("/", (req, res) => {
-    // Get the current directory by using fileURLToPath to handle ES6 import.meta.url
-    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// Serve the React app's static files
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, '../Frontend/build')));
 
-    // Correct the path to the frontend build directory
-    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+// Route to serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../Frontend/build', 'index.html'));
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
